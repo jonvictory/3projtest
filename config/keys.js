20d045b2eca94137e9824cad3f670244d03db04b
jonhuynh <jonhuynh@workfront.com>
2020-01-26 20:24:21 -0700

    this should be the working base model for individual user authentication using MONGO

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://j-admin:K!11er!sme!!@cluster0-ahjj3.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://j-admin:K!11er!sme!!@cluster0-ahjj3.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 }
